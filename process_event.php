e008dece46fea4ac84d86255e89c568ba9ebbbef
oliver bello <obello26@icloud.com>
2024-04-24 01:06:45 -0400

    concert info goes into database!!!!!!!!!!!

--- process_event.php
+++ process_event.php
@@ -7,12 +7,22 @@
 </head>
 <body>
     <?php
-       // $events = $_GET['allEvents'];
-       // echo $events;
-       echo "need to figure out how to get form data";
+        $events = array();
 
-       $myList = $_POST['addToList'];
-       echo $myList;
+        if(!empty($_POST['addToList'])) {
+            // Counting number of checked checkboxes.
+            $checked_count = count($_POST['addToList']);
+            echo "You have selected following ".$checked_count." option(s): <br/>";
+            // Loop to put identifier for each checked event into an array
+            foreach($_POST['addToList'] as $selected) {
+                echo "<p>".$selected ."</p>";
+                $events[] = $selected;
+            }
+            echo print_r($events);
+        } else {
+            echo "<b>Please Select Atleast One Option.</b>";
+        }
+        
 
         // Variables to set up connection
         $server = "localhost";
@@ -31,13 +41,33 @@
         // Select database
         $conn->select_db($db);
 
-        // https://phpdelusions.net/mysqli_examples/insert
-        // Create and prepare query
-        // $sql = "INSERT INTO `my_list`(`Name`, `Artist`, `Venue`, `Date`, `Time`, `City`, `State`, `Country`, `URL`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        // $statement = $conn->prepare($sql);
-        // Bind parameters and execute query
-        // $statement->bind_param("sssssssss", $eventName, $artistName, $venue, $date, $localTime, $city, $state, $country, $url);
-        // $statement->execute();
+        // For every event in array:
+        foreach ($events as $event) {
+            // Get csv data from hidden form field
+            $eventString = $_POST[$event];
+            // Explode csv into array
+            $event_data = explode(",", $eventString); 
+            
+            $eventName = $event_data[0];
+            $artistName = $event_data[1];
+            $venue = $event_data[2];
+            $date = $event_data[3];
+            $localTime = $event_data[4];
+            $city = $event_data[5];
+            $state = $event_data[6];
+            $country = $event_data[7];
+            $url = $event_data[8];
+
+            // https://phpdelusions.net/mysqli_examples/insert
+            // Create and prepare query
+            $sql = "INSERT INTO `my_list`(`Name`, `Artist`, `Venue`, `Date`, `Time`, `City`, `State`, `Country`, `URL`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            $statement = $conn->prepare($sql);
+            // Bind parameters and execute query
+            $statement->bind_param("sssssssss", $eventName, $artistName, $venue, $date, $localTime, $city, $state, $country, $url);
+            $statement->execute();
+        }
+
+        
     ?>
 </body>
 </html>
